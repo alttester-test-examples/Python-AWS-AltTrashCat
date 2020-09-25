23e768b6df664b78a40f40bf55d44c5a924b2f00
Gombos Kriszta <kriszta.gombos@altom.com>
2020-09-25 09:32:51 +0000

    Resolve "Update sample project with AWS to work with AltUnity Tester version 1.5.6."

---
+++ create-aws-package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Print how script is used and exit
+function print_help_and_die() {
+    echo "Usage: $0 <ios|android>"
+    exit 0
+}
+
+if [[ "$#" -eq 1 ]]; then
+    ENV="$1"
+else
+    print_help_and_die
+fi
+
+find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+
+TEST_FILE="test-package_${ENV}.zip"
+
+rm ${TEST_FILE}
+
+echo "Creating test file for environment: ${ENV}"
+zip -r a "${TEST_FILE}" tests/start_page_test.py tests/base_test.py pages/ requirements.txt 
+echo "
+You should now upload test file '${TEST_FILE}' to AWS Cloud"
--- tests/base_test.py
+++ tests/base_test.py
@@ -4,7 +4,7 @@ import time
 
 sys.path.append(os.path.dirname(__file__))
 
-from altunityrunner import AltrunUnityDriver
+from altunityrunner import AltUnityDriver
 import unittest
 import pytest
 import os
@@ -26,7 +26,7 @@ class TestBase(unittest.TestCase):
         print("Appium driver started")
         time.sleep(10)
         cls.setup_port_forwarding()
-        cls.altdriver = AltrunUnityDriver(cls.driver, cls.platform)
+        cls.altdriver = AltUnityDriver()
 
     @classmethod
     def setup_port_forwarding(cls):
@@ -41,10 +41,10 @@ class TestBase(unittest.TestCase):
 
         if cls.platform == 'android':
             AltUnityAndroidPortForwarding().forward_port_device()
-            print("Port forwared (Android).")
+            print("Port forwarded (Android).")
         else:
             AltUnityiOSPortForwarding().forward_port_device()
-            print("Port forwared (iOS).")
+            print("Port forwarded (iOS).")
 
     @classmethod
     def tearDownClass(cls):
